3b055f0dc1a852c2bf0c3e894bcbd075412f3c64
unknown <caloy0714@gmail.com>
2022-10-22 19:28:34 +0800

    rename

--- connection.php
+++ connection.php
@@ -1,13 +1,11 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=[mydb]";
-$user = "[user12]";
-$passwd = "[12user]";
+$dsn = "mysql:host=localhost;dbname=pdc10_classes";
+$user = "user123";
+$passwd = "passwd";
 
 $pdo = new PDO($dsn, $user, $passwd);
 
 $stm = $pdo->query("SELECT VERSION()");
 
 $version = $stm->fetch();
-
-//echo $version[0] . PHP_EOL;
